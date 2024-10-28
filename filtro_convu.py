1d4345e5390b93a4396cacb210c35ee8e2c829b4
Amilcar Rodríguez <amilcar13698@gmail.com>
2024-10-28 13:50:55 -0600

    Actualizacion: suavizado

--- filtro_convu.py
+++ filtro_convu.py
@@ -17,9 +17,21 @@ for i in range(x):
         #orig_y = int(j * scale_x)
         scaled_img[i*2, j*2] = img[i, j]
 
+mat_convu = np.ones((3,3), np.float32) / 9
+img_convu = np.zeros(scaled_img.shape, dtype=np.uint8)
+
+for i in range(1, scaled_img.shape[0] - 1):
+    for j in range(1, scaled_img.shape[1] - 1):
+        for c in range(1):
+            suma = 0
+            # Aplicar la matriz de convolución
+            for ki in range(-1, 2):
+                for kj in range(-1, 2):
+                    suma += scaled_img[i + ki, j + kj] * mat_convu[ki + 1, kj + 1]
+                    img_convu[i, j] = np.clip(suma, 0, 255) 
 
 cv.imshow ("img original", img)
 cv.imshow ("img escalada", scaled_img)
-#cv.imshow ("imagen 1/9", img_convu)
+cv.imshow ("imagen 1/9", img_convu)
 cv.waitKey(0)
 cv.destroyAllWindows
