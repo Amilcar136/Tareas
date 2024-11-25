8fc8d296330cdce7952d1820d646207bfc5103c3
Amilcar Rodríguez <amilcar13698@gmail.com>
2024-11-25 13:46:53 -0600

    Actualizacion triangulo de pascal con triangulos

--- Trian_pasc.py
+++ Trian_pasc.py
@@ -3,17 +3,26 @@ from OpenGL.GL import *
 from OpenGL.GLU import *
 
 
-def draw_triangle():
+def draw_triangle(x, y, color):
 
     glBegin(GL_TRIANGLES)
-    glColor3f(1.0, 0.0, 0.0)  # Rojo
-    glVertex2f(-0.5, -0.5)    # Vértice inferior izquierdo
-    glColor3f(0.0, 1.0, 0.0)  # Verde
-    glVertex2f(0.5, -0.5)     # Vértice inferior derecho
-    glColor3f(0.0, 0.0, 1.0)  # Azul
-    glVertex2f(0.0, 0.5)      # Vértice superior
+    glColor3f(color[0], color[1], color[2])  # Rojo
+    glVertex2f(x - 0.25, y - 0.25)    # Vértice inferior izquierdo
+    glVertex2f(x + 0.25, y + 0.5)     # Vértice inferior derecho
+    glVertex2f(x, y + 0.25)      # Vértice superior
     glEnd()
     
+def triang_Pasc(rows):
+    triangle = []
+    for i in range (rows):
+        for j in range (rows + i):
+            #Calcular posicion (x, y)
+            x = (j - i / 2) * 0.5
+            y = -i * 0.5
+
+            color_value = (j / (i + 1), 0.5, 1.0 - (j / (i+1)))
+            triangle.append((x, y, color_value))
+        return triangle
 
 def main():
     # Inicializar GLFW
@@ -32,23 +41,14 @@ def main():
     # Establecer el color de fondo
     glClearColor(0.0, 0.0, 0.0, 1.0)
 
-    #Proyeccion
-    glMatrixMode(GL_PROJECTION)
-    glLoadIdentity()
-    gluOrtho2D(-5, 5, -5, 5) #Rango de vista
+    triangulo_pascal = triang_Pasc(5)
+
 
     while not glfw.window_should_close(window):
         glClear(GL_COLOR_BUFFER_BIT)
-        positions = [
-        (-2, 0, 0),  #Triangulo 1
-        (2, -2, 0),   #Triangulo 2
-        ]
 
-        for pos in positions:
-            glPushMatrix()
-            glTranslatef(*pos)
-            draw_triangle()
-            glPopMatrix()
+        for (x, y, color) in triangulo_pascal:
+            draw_triangle(x, y, color)
 
         glfw.swap_buffers(window)
         glfw.poll_events()
